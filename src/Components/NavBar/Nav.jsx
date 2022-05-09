c8862e09c63ee18e9817bfaf63eff8822121b72b
Dunsin <jesudunsinfaiye@gmail.com>
2022-05-09 14:16:34 +0100

    ade it mobile friendly

--- src/Components/NavBar/Nav.jsx
+++ src/Components/NavBar/Nav.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Nav.css'
 
+import { GiHamburgerMenu } from 'react-icons/gi'
+
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(true)
+
+  const nav =
+    <ul>
+      <li>Home</li>
+      <li>Projects</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+
   return (
     <div className='nav'>
-      <h1>Favour</h1>
-      <ul>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+      <div>
+        <h1>Favour</h1>
+        <GiHamburgerMenu onClick={() => setIsOpen(!isOpen)} />
+        <div className='large' >
+          {nav}
+        </div>
+      </div>
+      <div className='small' style={{
+        display: isOpen ? 'none' : 'block'
+      }}>
+        {nav}
+      </div>
     </div>
   )
 }
