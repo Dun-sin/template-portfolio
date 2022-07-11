47052808c2db0c42edb94a3a419756c00eb55253
Dunsin <jesudunsinfaiye@gmail.com>
2022-07-11 20:38:41 +0100

    feat: removed fraction from projects

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Slide } from 'react-awesome-reveal';
 // components
 import Home from './Components/Home/Home';
 import Nav from './Components/NavBar/Nav';
-import Projects from './Components/Projects/Projects'
+import Projects from './Components/Projects/Projects';
 import Socials from './Components/Socials/Socials';
 import About from './Components/About/About';
 import Skills from './Components/Skills/Skills';
@@ -16,41 +16,41 @@ import Mail from './Components/Mail/Mail';
 import { CgArrowLongUp } from 'react-icons/cg';
 
 function App() {
-  return (
-    <div className='App'>
-      <div>
-        <Nav />
-        <Home />
-      </div>
-      <div className='dark'>
-        <Slide direction='right' triggerOnce fraction='0.1'>
-          <Projects />
-        </Slide>
-        <Slide triggerOnce>
-          <Socials />
-        </Slide>
-        <Slide direction='right' triggerOnce>
-          <About />
-        </Slide>
-        <Slide triggerOnce >
-          <Skills />
-        </Slide>
-      </div>
-      <div>
-        <Slide direction='right' triggerOnce>
-          <Mail />
-        </Slide>
-      </div>
-      <div className='dark'>
-        <Slide triggerOnce>
-          <p>Thank you for scrolling!</p>
-          <div className='arrow' onClick={() => animateScroll.scrollToTop()}>
-            <CgArrowLongUp />
-          </div>
-        </Slide>
-      </div>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div>
+				<Nav />
+				<Home />
+			</div>
+			<div className='dark'>
+				<Slide direction='right' triggerOnce>
+					<Projects />
+				</Slide>
+				<Slide triggerOnce>
+					<Socials />
+				</Slide>
+				<Slide direction='right' triggerOnce>
+					<About />
+				</Slide>
+				<Slide triggerOnce>
+					<Skills />
+				</Slide>
+			</div>
+			<div>
+				<Slide direction='right' triggerOnce>
+					<Mail />
+				</Slide>
+			</div>
+			<div className='dark'>
+				<Slide triggerOnce>
+					<p>Thank you for scrolling!</p>
+					<div className='arrow' onClick={() => animateScroll.scrollToTop()}>
+						<CgArrowLongUp />
+					</div>
+				</Slide>
+			</div>
+		</div>
+	);
 }
 
 export default App;
