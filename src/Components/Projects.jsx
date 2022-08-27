a28de9ce40a0b925ccaa19678320aeecbd9644ab
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2022-08-27 19:13:15 +0100

    feat: remove line break

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,7 +5,7 @@ import '../styles/Projects.css';
 const Projects = () => {
   const projectsInfo = {
     smart: {
-      name: 'Smart <br />Search',
+      name: 'Smart Search',
       desc: 'Find the best smartphone for you',
       liveLink: 'https://smartsearch-production.up.railway.app',
       githubLink: 'https://github.com/Dun-sin/smartsearch',
@@ -13,7 +13,7 @@ const Projects = () => {
       isOpenSource: true
     },
     anon: {
-      name: 'AnonChat <br />App',
+      name: 'AnonChat App',
       desc: 'An app to help you chat in secret',
       liveLink: 'https://anon-chat-app.vercel.app/',
       githubLink: 'https://github.com/Dun-sin/anon-chat-app',
