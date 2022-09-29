df75ca572b1f3e1966aa6691a699497b49b784e1
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2022-09-29 09:29:43 +0100

    Update Project.jsx

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -10,7 +10,7 @@ const Project = ({ info }) => {
         {info.techs.map(item => <div key={item}>{item}</div>)}
       </div>
       <div className="btns">
-        <a href={info.liveLink} target="_blank" rel="noreferrer">Github</a>
+        <a href={info.liveLink} target="_blank" rel="noreferrer">Live</a>
         <a href={info.githubLink} target="_blank" rel="noreferrer">Github</a>
       </div>
       {info.isOpenSource && <div className="open">Open Source</div>}
