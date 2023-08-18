b0c10be1d895fb8fb65502f1e13ad0a9fd42aac6
Schleidens <alectineschleidens2016@gmail.com>
2023-08-18 13:54:16 -0400

    fix responsive issue in project section and improve basecode

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import '../styles/Project.css'
+import React from "react";
+import "../styles/Project.css";
 
 const Project = ({ info }) => {
   return (
     <div className="project">
-      <h2>{info.name}</h2>
-      <p>{info.desc}</p>
-      <div className="tech-stack">
-        {info.techs.map(item => <div key={item}>{item}</div>)}
+      <div>
+        <h2>{info.name}</h2>
+        <p>{info.desc}</p>
+        <div className="tech-stack">
+          {info.techs.map((item, index) => (
+            <div key={index}>{item}</div>
+          ))}
+        </div>
+        <div className="btns">
+          <a href={info.liveLink} target="_blank" rel="noreferrer">
+            Live
+          </a>
+          <a href={info.githubLink} target="_blank" rel="noreferrer">
+            Github
+          </a>
+        </div>
+        {info.isOpenSource && <div className="open">Open Source</div>}
       </div>
-      <div className="btns">
-        <a href={info.liveLink} target="_blank" rel="noreferrer">Live</a>
-        <a href={info.githubLink} target="_blank" rel="noreferrer">Github</a>
-      </div>
-      {info.isOpenSource && <div className="open">Open Source</div>}
     </div>
-  )
-}
+  );
+};
 
 export default Project;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,35 +1,44 @@
-import React from 'react';
-import Project from './Project.jsx';
-import '../styles/Projects.css';
+import React from "react";
+import Project from "./Project.jsx";
+import "../styles/Projects.css";
 
 const Projects = () => {
-  const projectsInfo = {
-    one: {
-      name: 'Project 1',
-      desc: 'congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed',
-      liveLink: 'website url',
-      githubLink: 'https://github.com/',
-      techs: ['HTML', 'CSS', 'TypeScript'],
-      isOpenSource: true
+  const projectsInfo = [
+    {
+      name: "Project 1",
+      desc: "congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed",
+      liveLink: "website url",
+      githubLink: "https://github.com/",
+      techs: ["HTML", "CSS", "TypeScript"],
+      isOpenSource: true,
     },
-    two: {
-      name: 'Project 2',
-      desc: 'congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed',
-      liveLink: 'website url',
-      githubLink: 'https://github.com/',
-      techs: ['React', 'Node', 'MongoDB', 'Socket.io', 'Redux'],
-      isOpenSource: true
+    {
+      name: "Project 2",
+      desc: "congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed",
+      liveLink: "website url",
+      githubLink: "https://github.com/",
+      techs: ["React", "Node", "MongoDB", "Socket.io", "Redux"],
+      isOpenSource: true,
+    },
+    {
+      name: "Project 3",
+      desc: "congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed",
+      liveLink: "website url",
+      githubLink: "https://github.com/",
+      techs: ["React", "Node", "Express", "Redux"],
+      isOpenSource: true,
     }
-  }
+  ];
   return (
-    <div className="Projects" id="projects" >
+    <div className="Projects" id="projects">
       <h2>My Projects</h2>
       <div>
-        <Project info={projectsInfo.one} />
-        <Project info={projectsInfo.two} />
+        {projectsInfo.map((data, index) => (
+          <Project key={index} info={data} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
