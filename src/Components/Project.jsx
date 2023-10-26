5ac046128d2302451a53d7dfcc3b034922f0addd
Judge-Paul <judgepaulogebe3@gmail.com>
2023-10-26 20:53:38 +0100

    refactor: update projects info to be an array

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import '../styles/Project.css'
 
-const Project = ({ info }) => {
+const Project = ({ name, desc, techs, liveLink, githubLink, isOpenSource }) => {
   return (
     <div className="project">
-      <h2>{info.name}</h2>
-      <p>{info.desc}</p>
+      <h2>{name}</h2>
+      <p>{desc}</p>
       <div className="tech-stack">
-        {info.techs.map(item => <div key={item}>{item}</div>)}
+        {techs.map(item => <div key={item}>{item}</div>)}
       </div>
       <div className="btns">
-        <a href={info.liveLink} target="_blank" rel="noreferrer">Live</a>
-        <a href={info.githubLink} target="_blank" rel="noreferrer">Github</a>
+        <a href={liveLink} target="_blank" rel="noreferrer">Live</a>
+        <a href={githubLink} target="_blank" rel="noreferrer">Github</a>
       </div>
-      {info.isOpenSource && <div className="open">Open Source</div>}
+      {isOpenSource && <div className="open">Open Source</div>}
     </div>
   )
 }
 
 export default Project;
+
