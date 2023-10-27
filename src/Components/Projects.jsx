a186f1d24ec6d7ee827c11c074e1b9bcfe07c24b
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-10-27 09:45:25 +0100

    refactor: update projects info to be an array (#22)

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,35 +1,36 @@
-import React from 'react';
-import Project from './Project.jsx';
-import '../styles/Projects.css';
+import React from "react";
+import Project from "./Project.jsx";
+import "../styles/Projects.css";
 
 const Projects = () => {
-  const projectsInfo = {
-    one: {
-      name: 'Project 1',
-      desc: 'congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed',
-      liveLink: 'website url',
-      githubLink: 'https://github.com/',
-      techs: ['HTML', 'CSS', 'TypeScript'],
-      isOpenSource: true
+  const projectsInfo = [
+    {
+      name: "Project 1",
+      desc: "congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed",
+      liveLink: "website url",
+      githubLink: "https://github.com/",
+      techs: ["HTML", "CSS", "TypeScript"],
+      isOpenSource: true,
     },
-    two: {
-      name: 'Project 2',
-      desc: 'congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed',
-      liveLink: 'website url',
-      githubLink: 'https://github.com/',
-      techs: ['React', 'Node', 'MongoDB', 'Socket.io', 'Redux'],
-      isOpenSource: true
-    }
-  }
+    {
+      name: "Project 2",
+      desc: "congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed",
+      liveLink: "website url",
+      githubLink: "https://github.com/",
+      techs: ["React", "Node", "MongoDB", "Socket.io", "Redux"],
+      isOpenSource: true,
+    },
+  ];
   return (
-    <div className="Projects" id="projects" >
+    <div className="Projects" id="projects">
       <h2>My Projects</h2>
       <div>
-        <Project info={projectsInfo.one} />
-        <Project info={projectsInfo.two} />
+        {projectsInfo.map((project, index) => (
+          <Project key={index} {...project} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
